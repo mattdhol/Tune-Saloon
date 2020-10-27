84964389755c25840c8363dccbe6ce19bfe225d1
Matt D'Hollander <matthew@frontrowsolutions.com>
2020-10-27 10:56:51 -0400

    deploy to Heroku

--- config/database.js
+++ config/database.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/guitar', {
-
-  // mongodb+srv://Matt:@DutchBoi1@matt.jkqei.mongodb.net/Matt?retryWrites=true&w=majority
   
   useNewUrlParser: true,
   useUnifiedTopology: true,
