ce0417cd0d9c0dc0001f204054061e72dd68f774
Matt D'Hollander <matthew@frontrowsolutions.com>
2021-02-22 21:13:00 -0500

    favicon

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var session = require('express-session');
 var passport = require('passport');
 var methodOverride = require('method-override');
+var favicon = require('serve-favicon');
 require('./config/database');
 require('./config/passport');
 
@@ -34,6 +35,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 
 app.use('/', indexRouter);
