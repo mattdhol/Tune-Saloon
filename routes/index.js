8b8d556a3b58db3242cab80979fe6ba74134ab77
Matt D'Hollander <matthew@frontrowsolutions.com>
2021-12-30 13:14:41 -0500

    oauth, bypass

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,20 @@ var guitarCtrl = require('../controllers/guitar')
 router.get('/', guitarCtrl.intro_page)
 
 router.get('/songhome', guitarCtrl.song_home)
-router.get('/rejected', guitarCtrl.rejected)
+// router.get('/rejected', guitarCtrl.rejected)
 
-router.get('/songadd',isLoggedIn, guitarCtrl.song_add)
+router.get('/songadd', guitarCtrl.song_add)
 router.post('/songadd', guitarCtrl.song_create)
 
-router.get('/:id',isLoggedIn, guitarCtrl.song_progress)
+router.get('/:id', guitarCtrl.song_progress)
 router.post('/:id', guitarCtrl.song_lesson)
 
 router.delete('/delete/:id', guitarCtrl.song_delete)
 
 
-function isLoggedIn(req, res, next) {
-    if ( req.isAuthenticated() ) return next();
-    res.redirect('/rejected');
-  }
+// function isLoggedIn(req, res, next) {
+//     if ( req.isAuthenticated() ) return next();
+//     res.redirect('/rejected');
+//   }
 
 module.exports = router;
--- routes/practice.js
+++ routes/practice.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var practiceCtrl = require('../controllers/practice');
 const practice = require('../models/practice');
 
-router.get('/practicehome',isLoggedIn, practiceCtrl.practice_home)
+router.get('/practicehome', practiceCtrl.practice_home)
 router.post('/practiceadd', practiceCtrl.practice_add_post_req)
 
-router.get('/practiceadd',isLoggedIn, practiceCtrl.practice_add)
+router.get('/practiceadd', practiceCtrl.practice_add)
 
 router.get('/:id', practiceCtrl.practice_progress)
 router.post('/:id', practiceCtrl.chord_add_post_req)
